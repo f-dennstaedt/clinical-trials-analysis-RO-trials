fc0070f15c89050dbe9e2cbcd191da6f53aee90b
f-dennstaedt <fabiodennstaedt@gmx.de>
2026-08-26 10:44:14 +0200

    update

--- data-extraction-groundtruth.py
+++ data-extraction-groundtruth.py
@@ -2,8 +2,18 @@
 from data_element_extractor import DataElementExtractor
 
 dee = DataElementExtractor()
-dee.set_model(model_name="rombodawg/Rombos-LLM-V2.6-Qwen-14b", inference_type="transformers", model_type="Transformers", attn_implementation="eager", move_to_gpu=True, device_map="cuda:1")
+
+## Model ids used in the study
+#model_id="openai/gpt-oss-20b"
+#model_id="openai/gpt-oss-120b"
+model_id="Qwen/Qwen3.6-27B"
+
+dee.set_model(model_name=model_id, inference_type="transformers", model_type="Transformers", attn_implementation="eager", move_to_gpu=True, device_map="auto")
 dee.load_topics("data/topics.json")
 dee.show_topics_and_categories()
 dee.set_choice_symbols("alphabetical")
-dee.extract_from_table("data/trials-data-groundtruth.csv", with_evaluation=True, batch_size=1)
+dee.extract_from_table("data/trials-data-groundtruth.csv", constrained_output=False, with_evaluation=True, batch_size=1)
+
+
+
+
